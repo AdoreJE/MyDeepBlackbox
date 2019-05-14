5d573b152af13fb392b537e7b54f5b1a14a6c0a4
thgeLove <thkim4693@naver.com>
2019-05-14 09:54:12 +0900

    .

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import seaborn as sns
+# import seaborn as sns
 import cv2
-import pandas as pd
-import numpy as np
-import tensorflow as tf
+# import pandas as pd
+# import numpy as np
+# import tensorflow as tf
 from tensorflow import keras
 # from tensorflow.keras.layers import Conv2D, MaxPooling2D
 # from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
@@ -18,6 +18,7 @@ import numpy as np
 # from PIL import Image
 # from tensorflow.keras.applications import MobileNet
 from keras.applications.inception_v3 import InceptionV3
+from keras.applications.vgg16 import VGG16
 # from keras.preprocessing import image
 from keras.models import Model, load_model
 from keras.layers import Dense, GlobalAveragePooling2D
@@ -28,39 +29,40 @@ import glob, os
 
 
 # base_model = InceptionV3(weights='imagenet', input_shape = (150,150, 3),include_top=False)
-# x = base_model.output
-# x = GlobalAveragePooling2D()(x)
-# # let's add a fully-connected layer
-# x = Dense(1024, activation='relu')(x)
-# x=Dense(64, activation='relu')(x)
+base_model = VGG16(weights='imagenet', input_shape = (150,150, 3),include_top=False)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+x=Dense(64, activation='relu')(x)
 
-# # and a logistic layer -- let's say we have 200 classes
-# predictions = Dense(units = 2, activation = 'softmax')(x)
+# and a logistic layer -- let's say we have 200 classes
+predictions = Dense(units = 2, activation = 'softmax')(x)
 
-# # this is the model we will train
-# model = Model(inputs=base_model.input, outputs=predictions)
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
 
-# for layer in base_model.layers:
-#     layer.trainable = False
-# # model.compile(optimizer='adam', loss='binary_crossentropy',metrics = ['accuracy'])
-# model.compile(optimizer='adam', loss='categorical_crossentropy')
-# from keras.preprocessing.image import ImageDataGenerator
+for layer in base_model.layers:
+    layer.trainable = False
+# model.compile(optimizer='adam', loss='binary_crossentropy',metrics = ['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+from keras.preprocessing.image import ImageDataGenerator
 
-# train_datagen = ImageDataGenerator(rescale = 1./255,
-#                                    shear_range = 0.2,
-#                                    zoom_range = 0.2,
-#                                    horizontal_flip = True)
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
 
-# training_set = train_datagen.flow_from_directory('0510data/train',
-#                                                  target_size = (150,150),
-#                                                  batch_size = 32,
-#                                                  class_mode = 'categorical')
+training_set = train_datagen.flow_from_directory('0510data/train',
+                                                 target_size = (150,150),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
 
-# model.fit_generator(training_set,
-#                          steps_per_epoch = 1000,
-#                          epochs = 5)
+model.fit_generator(training_set,
+                         steps_per_epoch = 1000,
+                         epochs = 5)
 
-# model.save('0510data2adam1000.h5')
+model.save('0510dataVgg16.h5')
 
 # model=load_model('0306data2.h5')
 # for i, layer in enumerate(base_model.layers):
@@ -80,24 +82,24 @@ import glob, os
 
 
 
-model=load_model('0510data2adam1000.h5')
+# model=load_model('0510data2adam1000.h5')
 
-files = glob.glob(os.path.join('0510data/test/1', "*"))
-# filename = '28-0012.jpg'
-c=0
-for filename in files:
-    # print(filename)
-    img = cv2.imread(filename)
-    img = cv2.resize(img,(150,150))
-    img = np.float32(img / 255)
-    img = np.reshape(img,[1,150,150,3])
+# files = glob.glob(os.path.join('0510data/test/1', "*"))
+# # filename = '28-0012.jpg'
+# c=0
+# for filename in files:
+#     # print(filename)
+#     img = cv2.imread(filename)
+#     img = cv2.resize(img,(150,150))
+#     img = np.float32(img / 255)
+#     img = np.reshape(img,[1,150,150,3])
 
-    pred = model.predict(img)
-    # print(pred)
-    if pred[0][0] < pred[0][1]:
-        print('1')
-        c=c+1
-    else:
-        print('0')
-    print(pred)
-print('%d in %d'  %(c, len(files)))
+#     pred = model.predict(img)
+#     # print(pred)
+#     if pred[0][0] < pred[0][1]:
+#         print('1')
+#         c=c+1
+#     else:
+#         print('0')
+#     print(pred)
+# print('%d in %d'  %(c, len(files)))
